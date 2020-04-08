65245eb73b02ece04add91bfa9f62b76181bf90c
Jimmy <jimmy.kohl@epitech.eu>
2020-04-08 16:02:30 +0200

    [MOD] Finis clean repo and import sql database

---
+++ epytodo.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS epytodo;
+CREATE DATABASE epytodo;
+
+USE epytodo;
+CREATE TABLE "user"
+(
+    "user_id" INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    "username" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "task"
+(
+    "task_id" INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    "title" VARCHAR(10) NOT NULL,
+    "begin" TIMESTAMP DEFAULT UTC_TIMESTAMP,
+    "end" TIMESTAMP,
+    "status" ENUM("not started", "in progress", "done") DEFAULT "not started"
+);
+
+CREATE TABLE "user_has_task"
+(
+    "fk_user_id" VARCHAR(50) NOT NULL,
+    "fk_task_id" VARCHAR(50) NOT NULL
+);
